3a21694c5f6b2c8f4467e8376a7772538035e26c
Nate <nate.rosenstock@hakai.org>
2020-01-28 13:38:49 -0800

    add DasDds.sh, GenerateDatasetsXml.sh

---
+++ DasDds.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker run --rm -it \
+  -v "$(pwd)/datasets:/datasets" \
+  -v "$(pwd)/logs:/erddapData/logs" \
+  -v "$(pwd)/erddap/content:/usr/local/tomcat/content/erddap" \
+  axiom/docker-erddap:latest \
+  bash -c "cd webapps/erddap/WEB-INF/ && bash DasDds.sh"
+  
