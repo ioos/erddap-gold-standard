e5f0454ceba4ac75b87b5ac6b6e1fcef781107e8
jeffcullis <jcullis@dal.ca>
2020-10-05 17:51:31 -0300

    Add command to copy MCTD sample dataset for BIO ODI.

---
+++ scripts/bio_odi.sh
@@ -0,0 +1,3 @@
+# Assuming the correct branch is checked out
+cp /opt/cioos-siooc_data_transform/cioos_data_transform/odi_data/sample_data/2020-03-06/MCTD_HUD2013004_1845_10515_1800.nc /opt/provisioning/erddap-gold-standard/datasets
+
