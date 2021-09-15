1c70356703ff04c3753a8f1890f14e97e9f116cd
jeffcullis <jcullis@dal.ca>
2021-09-15 11:17:08 -0300

    Update GenerateDatasetsXml.sh and docker-compose.yml to use the DATASETS_DIR specified in the .env file.

--- GenerateDatasetsXml.sh
+++ GenerateDatasetsXml.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+if [ -e "$(pwd)/.env" ]; then
+  DATASETS_DIR=`grep DATASETS_DIR $(pwd)/.env | sed s/.*\=//`
+fi
+if [ -z $DATASETS_DIR ]; then
+  DATASETS_DIR=$(pwd)/datasets
+fi
 docker run --rm -it \
-  -v "$(pwd)/datasets:/datasets" \
+  -v "${DATASETS_DIR}:/datasets" \
   -v "$(pwd)/logs:/erddapData/logs" \
   axiom/docker-erddap:latest \
   bash -c "cd webapps/erddap/WEB-INF/ && bash GenerateDatasetsXml.sh -verbose"
