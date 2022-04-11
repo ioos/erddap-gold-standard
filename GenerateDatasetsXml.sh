6528ef1b3e94f9a6b7c4c9294c2ac976a2656b6f
Kyle Wilcox <kyle@axiomdatascience.com>
2022-04-11 12:53:58 -0400

    Update to ERDDAP 2.18, configure with a config.sh script

--- GenerateDatasetsXml.sh
+++ GenerateDatasetsXml.sh
@@ -2,6 +2,5 @@
 docker run --rm -it \
   -v "$(pwd)/datasets:/datasets" \
   -v "$(pwd)/logs:/erddapData/logs" \
-  axiom/docker-erddap:latest \
+  axiom/docker-erddap:2.18 \
   bash -c "cd webapps/erddap/WEB-INF/ && bash GenerateDatasetsXml.sh -verbose"
-  
