93ee73376214abf8169e0d550bb18abcf52f5c1d
Jacob Thompson <jacob.thompson@wiwasolvet.ca>
2022-05-09 15:10:45 -0300

    Bringing latest changes to keep dev consistent with IOOS master/CIOOS Atlantic master to test on dev server.

--- GenerateDatasetsXml.sh
+++ GenerateDatasetsXml.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 source $(pwd)/.env
-if [ -z $DATASETS_DIR ]; then
-  DATASETS_DIR=/srv/data/erddap
-fi
-if [ -z $IMAGE ]; then
-  IMAGE=cioosatlantic/docker-erddap:latest
+if [[ -z $DATASETS_DIR ]] || [[ -z $IMAGE ]]; then
+  echo "Error: DATASETS_DIR and/or IMAGE not set in docker compose .env file. Exiting."
+  exit 1
 fi
 docker run --rm -it \
   -v "${DATASETS_DIR}:/datasets" \
