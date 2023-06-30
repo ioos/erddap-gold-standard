c0948f41df1afc95ff0b67e8615010c3a08f9938
Callum Rollo <c.rollo@outlook.com>
2023-06-30 09:38:00 +0200

    use erddap 2.23

--- GenerateDatasetsXml.sh
+++ GenerateDatasetsXml.sh
@@ -2,5 +2,5 @@
 docker run --rm -it \
   -v "$(pwd)/datasets:/datasets" \
   -v "$(pwd)/logs:/erddapData/logs" \
-  axiom/docker-erddap:2.18 \
+  axiom/docker-erddap:2.23-jdk17-openjdk \
   bash -c "cd webapps/erddap/WEB-INF/ && bash GenerateDatasetsXml.sh -verbose"
