af562fd72e3dacdb1d9929528bbffdc60082d7c7
Callum Rollo <c.rollo@outlook.com>
2023-07-31 17:17:23 +0200

    bind content directory

--- GenerateDatasetsXml.sh
+++ GenerateDatasetsXml.sh
@@ -2,5 +2,6 @@
 docker run --rm -it \
   -v "$(pwd)/datasets:/datasets" \
   -v "$(pwd)/logs:/erddapData/logs" \
+   -v $(pwd)/erddap/content:/usr/local/tomcat/content/erddap \
   axiom/docker-erddap:2.23-jdk17-openjdk \
   bash -c "cd webapps/erddap/WEB-INF/ && bash GenerateDatasetsXml.sh -verbose"
