b602d384b167c8ff31b1bc3d460dadd3a6f2fdd2
Shane St Savage <shane@axds.co>
2026-04-20 22:20:04 -0700

    Allow argument passing to DasDds.sh and GenerateDatasetsXml.sh scripts
    Thanks @jklymak for original request/implementation, closes #77

--- GenerateDatasetsXml.sh
+++ GenerateDatasetsXml.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 docker run --rm -it \
   -v "$(pwd)/datasets:/datasets" \
   -v "$(pwd)/logs:/erddapData/logs" \
   -v "$(pwd)/erddap/content:/usr/local/tomcat/content/erddap" \
   -e ERDDAP_flagKeyKey=generate_datasets \
   erddap/erddap:latest \
-  bash -c "cd webapps/erddap/WEB-INF/ && bash GenerateDatasetsXml.sh -verbose"
+  bash -c "cd webapps/erddap/WEB-INF/ && bash GenerateDatasetsXml.sh -verbose $@"
